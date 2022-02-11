2b8211c59e23553eb4a8acbb13e51eb0b6b8971e
Charim-Cibersegurata <80909442+Charim-Cibersegurata@users.noreply.github.com>
2022-02-11 21:30:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,4 @@ import cv2
 
 st.title("My first Streamlit app")
 st.write("Hello, world")
-ctx = webrtc_streamer(
-    key="example",
-    video_processor_factory=VideoProcessor,
-    rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    }
-)
+
