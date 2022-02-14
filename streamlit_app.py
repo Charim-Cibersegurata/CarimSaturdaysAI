b646c8c1a13544a2b87e46b4791ceb1e14fecae8
Charim-Cibersegurata <80909442+Charim-Cibersegurata@users.noreply.github.com>
2022-02-14 20:37:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ def main():
     # Face Analysis Application #
     st.title("Real Time Face Emotion Detection Application")
     st.write("Click on start to use webcam and detect your face emotion. [Github repo](https://github.com/Charim-Cibersegurata/CarimSaturdaysAI/)")
-    webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION, video_processor_factory=Faceemotion)
+    webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV, video_processor_factory=Faceemotion)
 if __name__ == "__main__":
     main()
 
